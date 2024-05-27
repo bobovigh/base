40f63a196c7f1d22a45bb07e321c7aba18ac4872
Fodor Eniko <fodor.eniko.anna@gmail.com>
2024-05-27 17:54:19 +0200

    created tests for TrainSensor alarmState

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,16 +4,43 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
 
 public class TrainSensorTest {
+    TrainController mockTC;
+    TrainUser mockTU;
+    TrainSensorImpl trainSensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTC = mock(TrainController.class);
+        mockTU = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTC, mockTU);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void TestLowerLimit() {
+        trainSensor.overrideSpeedLimit(0);
+        verify(mockTU, times(1)).setAlarmState(false);
+    }
+
+    @Test
+    public void TestUpperLimit() {
+        trainSensor.overrideSpeedLimit(500);
+        verify(mockTU, times(1)).setAlarmState(false);
+    }
+
+    @Test
+    public void TestBelowLimit() {
+        trainSensor.overrideSpeedLimit(-1);
+        verify(mockTU, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void TestAboveLimit() {
+        trainSensor.overrideSpeedLimit(501);
+        verify(mockTU, times(1)).setAlarmState(true);
     }
 }
